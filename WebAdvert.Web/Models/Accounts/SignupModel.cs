e7c1d8ca6051c8430fdb04f1af012015f28c6a90
Nathan Schrader <schradermade@Nathans-MacBook-Pro.local>
2021-02-23 17:16:37 -0800

    add file structure

---
+++ WebAdvert.Web/Models/Accounts/SignupModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAdvert.Web.Models.Accounts
+{
+    public class SignupModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(6, ErrorMessage = "Password must be six characters in length")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords dont match!")]
+
+        public string ConfirmPassword { get; set; }
+    }
+}
