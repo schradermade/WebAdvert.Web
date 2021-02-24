bb219f234fc659253b2f084d871a1c8d2ff652e9
Nathan Schrader <schradermade@Nathans-MacBook-Pro.local>
2021-02-24 10:54:48 -0800

    add CognitoAttribute.Name

--- WebAdvert.Web/Controllers/Accounts.cs
+++ WebAdvert.Web/Controllers/Accounts.cs
@@ -6,6 +6,8 @@ using Amazon.Extensions.CognitoAuthentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebAdvert.Web.Models.Accounts;
+using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Runtime.Internal.Transform;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,9 +15,13 @@ namespace WebAdvert.Web.Controllers
 {
     public class Accounts : Controller
     {
+
+        // Properties
         private readonly SignInManager<CognitoUser> _signInManager;
         private readonly UserManager<CognitoUser> _userManager;
         private readonly CognitoUserPool _pool;
+
+        // Constructor
         public Accounts(SignInManager<CognitoUser> signInManager, UserManager<CognitoUser> userManager, CognitoUserPool pool)
         {
             _signInManager = signInManager;
@@ -23,14 +29,34 @@ namespace WebAdvert.Web.Controllers
             _pool = pool;
 
         }
+
+        // Route Handlers
         public async Task<IActionResult> Signup()
         {
-            return View();
+            var model = new SignupModel();
+            return View(model);
         }
 
         [HttpPost]
         public async Task<IActionResult> Signup(SignupModel model)
         {
+            if (ModelState.IsValid)
+            {
+                var user = _pool.GetUser(model.Email);
+                if (user.Status != null)
+                {
+                    ModelState.AddModelError("UserExists", "User with this email already exists");
+                    return View(model);
+                }
+                                                        // Correct??
+                user.Attributes.Add(CognitoAttribute.Name.ToString(), model.Email);
+                var createdUser = await _userManager.CreateAsync(user, model.Password);
+
+                if (createdUser.Succeeded)
+                {
+                    RedirectToAction("Confirm");
+                }
+            }
             return View();
         }
     }
