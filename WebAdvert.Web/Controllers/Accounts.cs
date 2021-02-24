a1fdb59983539629552afba4d55bd1598cbaf938
Nathan Schrader <schradermade@Nathans-MacBook-Pro.local>
2021-02-23 17:36:57 -0800

    add private properties

--- WebAdvert.Web/Controllers/Accounts.cs
+++ WebAdvert.Web/Controllers/Accounts.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WebAdvert.Web.Models.Accounts;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,8 +13,23 @@ namespace WebAdvert.Web.Controllers
 {
     public class Accounts : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _pool;
+        public Accounts(SignInManager<CognitoUser> signInManager, UserManager<CognitoUser> userManager, CognitoUserPool pool)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _pool = pool;
+
+        }
+        public async Task<IActionResult> Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup(SignupModel model)
         {
             return View();
         }
